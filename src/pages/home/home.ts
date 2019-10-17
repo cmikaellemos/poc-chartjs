3fb320a7129980d7e22c433de8d401cf931c33a4
Mikael Lemos <carlos.soares@portoseguro.com.br>
2019-10-17 14:51:45 -0300

    poc chartjs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,122 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core'
+import { Chart } from "chart.js";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  @ViewChild("barCanvas") barCanvas: ElementRef;
+  @ViewChild("doughnutCanvas") doughnutCanvas: ElementRef;
+  @ViewChild("lineCanvas") lineCanvas: ElementRef;
 
-  constructor(public navCtrl: NavController) {
+  private barChart: Chart;
+  private doughnutChart: Chart;
+  private lineChart: Chart;
 
+  constructor(
+  ) {}
+
+  ngOnInit() {
+    this.load()
   }
 
+  load() {
+    setTimeout(() => {
+      let data: any = [Math.random() * 100, Math.random() * 100, Math.random() * 100, Math.random() * 100, Math.random() * 100, Math.random() * 100]
+      this.barChart = new Chart(this.barCanvas.nativeElement, {
+        type: "bar",
+        data: {
+          labels: ["Vermelho", "Azul", "Amarelo", "Verde", "Roxo", "Laranja"],
+          datasets: [
+            {
+              label: "# de Votos",
+              data: data,
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)"
+              ],
+              borderColor: [
+                "rgba(255,99,132,1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)"
+              ],
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true
+                }
+              }
+            ]
+          }
+        }
+      });
+  
+      this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+        type: "doughnut",
+        data: {
+          labels: ["Vermelho", "Azul", "Amarelo", "Verde", "Roxo", "Laranja"],
+          datasets: [
+            {
+              label: "# de Votos",
+              data: data,
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)"
+              ],
+              hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#FF6384", "#36A2EB", "#FFCE56"]
+            }
+          ]
+        }
+      });
+  
+      this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+        type: "line",
+        data: {
+          labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"],
+          datasets: [
+            {
+              label: "Meu dataset",
+              fill: false,
+              lineTension: 0.1,
+              backgroundColor: "rgba(75,192,192,0.4)",
+              borderColor: "rgba(75,192,192,1)",
+              borderCapStyle: "butt",
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: "miter",
+              pointBorderColor: "rgba(75,192,192,1)",
+              pointBackgroundColor: "#fff",
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: "rgba(75,192,192,1)",
+              pointHoverBorderColor: "rgba(220,220,220,1)",
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: data,
+              spanGaps: false
+            }
+          ]
+        }
+      });
+    }, 2000)
+  }
 }
